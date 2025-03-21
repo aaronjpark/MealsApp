0bbd6d1ba829f64bd47dcd8f2c841a2491622c3f
Aaron Park <aaronj.park0304@gmail.com>
2025-03-21 01:37:40 -0500

    Create MealItem Component

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,15 +1,23 @@
-import { View, Text, StyleSheet } from "react-native";
-import { useRoute } from "@react-navigation/native";
+import { View, FlatList, StyleSheet } from "react-native";
 
 import { MEALS } from "../data/dummy-data";
 
 function MealsOverviewScreen({ route }) {
-  const route = useRoute();
   const catId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  function renderMealItem(itemData) {
+    return <View>
+      <Text></Text>
+    </View>
+  }
 
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen</Text>
+      <FlatList data={displayedMeals} keyExtractor={(item) => item.id} renderItem={renderMealItem}/>
     </View>
   );
 }
