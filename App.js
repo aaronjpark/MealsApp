d5963999c4da2607f6ae794c2c5d75e809695b65
Aaron Park <aaronj.park0304@gmail.com>
2025-03-21 00:45:01 -0500

    Create MealsOverviewScreen.js

--- App.js
+++ App.js
@@ -1,15 +1,24 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
-import CategoriesScreen from "./screens/CategoriesScreen";
 import * as SystemUI from "expo-system-ui";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import CategoriesScreen from "./screens/CategoriesScreen";
 
 SystemUI.setBackgroundColorAsync("#24180f");
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
-      <StatusBar style="light" />
-      <CategoriesScreen />
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
